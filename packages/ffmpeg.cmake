88f03e81f2470a99260553071dbe86a82201b34f
Predidit <34627277+Predidit@users.noreply.github.com>
2026-09-04 21:17:53 +0800

    build: update media stack to current mainline

--- packages/ffmpeg.cmake
+++ packages/ffmpeg.cmake
@@ -24,14 +24,13 @@ ExternalProject_Add(ffmpeg
         libvpl
         libjxl
         libxml2
-        shaderc
         libplacebo
         dav1d
         openal-soft
     GIT_REPOSITORY https://github.com/FFmpeg/FFmpeg.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--sparse --filter=tree:0"
-    GIT_TAG release/7.1
+    GIT_TAG 818e5d965be955be8842ee3a4cdd7b43ab81661d
     GIT_CLONE_POST_COMMAND "sparse-checkout set --no-cone /* !tests/ref/fate"
     PATCH_COMMAND ${EXEC} git apply ${CMAKE_CURRENT_SOURCE_DIR}/ffmpeg-*.patch
     UPDATE_COMMAND ""
@@ -105,7 +104,6 @@ ExternalProject_Add(ffmpeg
         --enable-libsoxr
         --enable-libspeex
         --enable-libmysofa
-        --enable-libshaderc
         --enable-libfribidi
         --enable-libfreetype
 
